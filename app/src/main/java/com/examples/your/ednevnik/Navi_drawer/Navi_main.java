e3735e8181ff36de28e3469354c9b2a341509068
isesar <ivan.sesar95@gmail.com>
2017-05-31 01:48:50 +0200

    Navigation drawer

---
+++ app/src/main/java/com/examples/your/ednevnik/Navi_drawer/Navi_main.java
@@ -0,0 +1,37 @@
+package com.examples.your.ednevnik.Navi_drawer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.examples.your.ednevnik.R;
+
+public class Navi_main extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi_main);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
