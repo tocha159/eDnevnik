c6655ae76f96de8582411b41186b8979a8bf8796
tomo <tomo.pinjuh@gmail.com>
2017-05-31 13:59:54 +0200

    Dodatna konfiguracija drawer-a i navigation view-a

--- app/src/main/java/com/examples/your/ednevnik/Profesor/Navi_main.java
+++ app/src/main/java/com/examples/your/ednevnik/Profesor/Navi_main.java
@@ -1,16 +1,22 @@
 package com.examples.your.ednevnik.Profesor;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.examples.your.ednevnik.R;
 
-public class Navi_main extends AppCompatActivity {
+public class Navi_main extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
 
     @Override
@@ -22,6 +28,12 @@ public class Navi_main extends AppCompatActivity {
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
@@ -32,5 +44,29 @@ public class Navi_main extends AppCompatActivity {
         if(mToggle.onOptionsItemSelected(item)){
             return true;
         }
-        return super.onOptionsItemSelected(item);    }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager fm=getFragmentManager();
+        int id = item.getItemId();
+
+        if (id == R.id.add_subject) {
+        }else if (id == R.id.add_student) {
+
+        }else if (id == R.id.view_student) {
+
+        }else if (id == R.id.id_ocjeni) {
+
+        }else if (id == R.id.id_zakljuci) {
+
+        }else if (id == R.id.id_izostanci) {
+
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
